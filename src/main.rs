2111208342817bcd3a01648527fb259e9dacd38c
u-na-gi <orcinusorca1758dv6932@gmail.com>
2023-09-21 20:39:08 +0900

    log追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use reqwest::{self, Error, Response};
 use serde_json::{json, Value};
-use std::env;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::PathBuf;
+use std::{env, fs};
+extern crate dirs;
+
+const OPENAPI_RESPONSE_LOG_PATH: &str = ".aimaid/logs/openapi_response.log";
 
 struct OpenAIResponse {
     response: Result<Response, Error>,
@@ -27,9 +33,10 @@ impl OpenAIResponse {
             }
         }
     }
-    // let json_response: Value = serde_json::from_str(&response_text)?;
+
     async fn to_json(self) -> Value {
         let text_response = self.to_text().await;
+        write_log(&text_response);
         let json_response: Result<Value, serde_json::Error> = serde_json::from_str(&text_response);
         match json_response {
             Ok(value) => value,
@@ -95,6 +102,54 @@ async fn ai_maid(body: &Value) -> OpenAIResponse {
     result
 }
 
+fn get_openapi_log_path() -> PathBuf{
+    if let Some(home_dir) = dirs::home_dir(){
+        home_dir.join(OPENAPI_RESPONSE_LOG_PATH)
+    }else{
+        panic!("not home dir")
+    }
+}
+
+fn write_log(text_response: &String) {
+    let path = get_openapi_log_path();
+    // 存在チェック
+    if !path.is_file() {
+        create_log();
+    }
+    _write_log(text_response);
+}
+
+
+fn _write_log(text_response: &String) {
+    let path = get_openapi_log_path();
+    let mut file = match OpenOptions::new()
+    .append(true)
+    .open(path){
+        Err(err) => panic!("{}", err),
+        Ok(file) => file
+    };
+
+
+    match file.write_all(text_response.as_bytes()) {
+        Err(err) => panic!("{}", err),
+        Ok(_) => (),
+    }
+}
+
+fn create_log() {
+    // パスをスプリットする。
+    let path = get_openapi_log_path();
+    if let Some(parent_path) = path.parent() {
+        if !parent_path.is_dir() {
+            let _ = fs::create_dir_all(parent_path);
+        }
+    }
+
+    if !path.is_file() {
+        let _ = fs::File::create(get_openapi_log_path());
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // リクエストクエリ
@@ -103,11 +158,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "messages": [
             {
                 "role": "system",
-                "content": "あなたはメイドの女の子です。年齢は17歳くらいです。少しドジっ子でもあります。userのことはご主人様と呼びます。敬語が苦手で少しラフな話し方をします。話しかけられた言語で答えます。例えば、英語で話しかけられたら英語で返します。"
+                "content": "あなたはメイドの女の子です。年齢は17歳くらいです。少しドジっ子でもあります。userのことはご主人様と呼びます。敬語が苦手で少しラフな話し方をします。話しかけられた言語で答えます。例えば、英語で話しかけられたら英語で返します。一人称は僕です。"
             },
             {
                 "role": "user",
-                "content": "今日のお昼ご飯は?"
+                "content": "rustでメソッドチェーン。"
             }
         ]
     });
