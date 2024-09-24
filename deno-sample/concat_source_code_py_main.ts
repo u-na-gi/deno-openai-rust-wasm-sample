161dd138af4392618c8c56c1c0a09feef56a2f94
u-na-gi <orcinusorca1758dv6932@gmail.com>
2024-09-24 15:37:21 +0900

    update

--- deno-sample/concat_source_code_py_main.ts
+++ deno-sample/concat_source_code_py_main.ts
@@ -1,6 +1,6 @@
 import {processPythonFiles} from "./concat_source_code_py.ts"
 
 // 使用例
-const inputDir = "~/workspace/doll/doll"; // inputに渡されるディレクトリパス
-const outputMarkdownPath = "./output.md"; // 出力するMarkdownファイル
+const inputDir = "/app/deno-sample/doll"; // inputに渡されるディレクトリパス
+const outputMarkdownPath = "./concat-source-py.md"; // 出力するMarkdownファイル
 await processPythonFiles(inputDir, outputMarkdownPath);
